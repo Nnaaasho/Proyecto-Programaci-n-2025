8b3d7767000507f5fee74f2fb8c0526b99aba8c5
Nnaaasho <nachiitoo2007@gmail.com>
2025-03-27 14:41:21 -0300

    Trabajamos con variables y buscamos los errores que habia que quitar o agregar

--- hola.php
+++ hola.php
@@ -1,1 +1,27 @@
-<?php echo "Hola mundo!";?>
+<?php echo "Hola mundo!";
+// hola mundo //
+/* hello world
+hola mundo */
+$nombre = "Manolo"; // Texto. Puede ser con comillas simples o dobles (String) //
+
+$edad = 31; // Enteros (Integer) //
+
+$altura = 1.72; // Decimales, usando el punto en lugar de la coma (Float) //
+
+$mayorEdad = true; // Verdad o falso (Boolean) //
+
+$nombreCompleto = "Juan Pérez"; // Agregue punto y coma // 
+
+$Edad = 25;
+
+$estAtura = 1.75;
+
+$numero = 10; // Saque las comillas ya que numero vendria a hacer un entero //
+
+$Apellido = "Fernandez"; // Cambie las comillas ´´ por "" por error de syntaxis //
+
+$pais_de_origen = "Uruguay"; // Agregue punto y coma //
+
+$salario = 2000; // Quite el signo "$" adelante del 2000 por error de syntaxis //
+
+?>
