0682e87373fe7b44131bbe762414ee8342b95079
Nnaaasho <nachiitoo2007@gmail.com>
2025-08-14 15:41:21 -0300

    empecemos con javascript, usando funciones

---
+++ JavaScript/practica.js
@@ -0,0 +1,18 @@
+var nombre = "Ana";
+let edad = 25;
+const PI = 3.1416;
+let radio = prompt ("Cuanto mide el radio");
+let radioEnNumero = parseInt (radio);
+
+function calcularCirculo(radio){
+    Math.PI;
+    let resultado = (radio ** 2) / Math.PI;
+    return resultado;
+}
+console.log(calcularCirculo(10));
+
+function calcularRectangulo(ancho, alto){
+
+    
+}
+
